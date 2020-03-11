98be0db837730b8bdfc0e05371be14b3c9e560ad
Eladio <eladiojuniorrodriguez@gmail.com>
2020-03-11 10:11:28 -0400

    Initial Commit

---
+++ Inverter Queue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inverter_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int cantidad = 0;
+            do
+            {
+                Console.Write("Inserte la cantidad de elementos de la cola: ");
+                cantidad = int.Parse(Console.ReadLine());
+            } while (cantidad <= 0);
+            Queue queue = new Queue();
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write($"Elemento[{i + 1}]: ");
+                int value = int.Parse(Console.ReadLine());
+                queue.enconlar(value);
+            }
+
+            Console.Write("Elemento N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+            int x = queue.cantidad();
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(queue.tope().Value);
+                queue.desencolar();
+            }
+            Queue queue1 = new Queue();
+            for (int i = 0; i < x; i++)
+            {
+                if (i < n)
+                    queue1.enconlar(stack.Pop());
+                else
+                {
+                    queue1.enconlar(queue.tope().Value);
+                    queue.desencolar();
+                }
+            }
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write($"{queue1.tope().Value}<-");
+                queue1.desencolar();
+            }
+        }
+    }
+}
---
+++ Inverter Queue/Queue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inverter_Queue
+{
+    class Queue
+    {
+        LinkedList<int> lista = new LinkedList<int>();
+        LinkedListNode<int> current;
+
+        public LinkedList<int> enconlar(int i)
+        {
+            if (lista.Count == 0)
+            {
+                lista.AddFirst(i);
+                current = lista.Last;
+            }
+            else
+            {
+                lista.AddAfter(current, i);
+                current = lista.Last;
+            }
+            return lista;
+        }
+
+        public LinkedList<int> desencolar()
+        {
+            lista.RemoveFirst();
+            return lista;
+        }
+
+        public int cantidad()
+        {
+            return lista.Count;
+        }
+
+        public LinkedListNode<int> tope()
+        {
+            return lista.First;
+        }
+    }
+}
